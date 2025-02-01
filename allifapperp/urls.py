94da824bee1fe9f17fb61ed3b8ad319e5e6778e8
Ahmed Muse <ahmeddove114@gmail.com>
2025-02-01 12:57:36 +0300

    usersapp added

--- allifapperp/urls.py
+++ allifapperp/urls.py
@@ -15,44 +15,11 @@ urlpatterns = [
 
 
     #since login is doing all the authentication, we need the two lines below
-    path('login/', include('django.contrib.auth.urls')),#this ensures and allows that we use the already built in authentication system
+    #path('login/', include('django.contrib.auth.urls')),#this ensures and allows that we use the already built in authentication system
     #path('login/', include('login.urls')),#point to login
 
  
-     
-     #path('logistics/', include('logistics.urls')),
-
-     #path('quotation/', include('dashboard.urls')),
-     #path('quotation/', include('quotation.urls')),
-      #path('allifmaal-erp/subscriptions/', include('allifapp.urls')),
-
-     
-     #path('invoice/', include('dashboard.urls')),
-     #path('invoice/', include('invoice.urls')),
-
-     #path('allifweb/', include('dashboard.urls')),
-     #path('allifweb/', include('allifweb.urls')),
-
-     #path('todoapp/', include('todoapp.urls')),
-     
-     #path('stockmanagementapp/', include('stockmanagementapp.urls')),
-     #path('hrmapp/', include('hrmapp.urls')),
-
-     #path('crmapp/', include('crmapp.urls')),
-     #path('flights/', include('flights.urls')),
-     #path('learningapp/', include('learningapp.urls')),
-     #path('ecommerceapp/', include('ecommerceapp.urls')),
-     #path('blogapp/', include('blogapp.urls')),
-     #path('shaafiapp/', include('shaafiapp.urls')),
-     #path('ilmapp/', include('ilmapp.urls')),
-     #path('django_ajax_crud/', include('django_ajax_crud.urls')),
-
-    #path('allifmaalapp/', include('allifmaalapp.urls')),
-    #path('hotelsapp/', include('hotelsapp.urls')),
-    #path('products/', include('products.urls')),
-    #path("select2/", include("django_select2.urls")),
-    #path('realestate/', include('realestate.urls')),
-    #path('Allifmaal/Application/For/Users/And/Accounts/Management/', include('allifmaalusersapp.urls')),
+    path('Allifmaal/Application/For/Users/And/Accounts/Management/', include('allifmaalusersapp.urls')),
     #path('Allifmaal/Application/Users/Login/App/Managers/', include('allifmaalloginapp.urls')),
     #path('Allifmaal/User/Interface/App', include('allifmaaluiapp.urls')),
     #path('Allifmaal/ERP/System/Admin/App/', include('allifmaaladminapp.urls')),
