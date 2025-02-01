5e4fdcd09c56dc8cd12630f9e2d131f70924e7f5
Ahmed Muse <ahmeddove114@gmail.com>
2025-02-01 12:17:38 +0300

    added apps

---
+++ allifmaaladminapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AllifmaaladminappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'allifmaaladminapp'
---
+++ allifmaalcommonapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AllifmaalcommonappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'allifmaalcommonapp'
