6cd621a2ab09da03d29b283ff255838bb2f48fc2
Ahmed Muse <ahmeddove114@gmail.com>
2025-02-02 17:36:05 +0300

    added domains

--- allifapperp/settings.py
+++ allifapperp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8*_cv^aw$^%(*k5zfx1+(svx3yw!448%^=ci6auje3ucz7gvn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['ahmeddove.pythonanywhere.com','localhost:8000']
+ALLOWED_HOSTS = ['ahmeddove.pythonanywhere.com','localhost']
 
 
 # Application definition
