26724680ca86b45e72bc12616ef9135ec59fa79a
Ahmed Muse <ahmeddove114@gmail.com>
2025-02-22 19:59:38 +0300

    test deploy

--- allifapperp/settings.py
+++ allifapperp/settings.py
@@ -246,7 +246,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT='static'
-STATICFILES_LOCATION='static'
+#STATICFILES_LOCATION='static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
