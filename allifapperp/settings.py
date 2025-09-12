a79d905b3af35a753f83d237a55b67230fec1653
Ahmed Muse <ahmeddove114@gmail.com>
2025-09-12 22:36:46 +0300

    airline tickets

--- allifapperp/settings.py
+++ allifapperp/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent # This variable correctly poin
 #SECRET_KEY = 'django-insecure-c8*_cv^aw$^%(*k5zfx1+(svx3yw!448%^=ci6auje3ucz7gvn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # Add a setting for maintenance mode
 
 #if DEBUG:
@@ -184,7 +184,6 @@ AUTO_LOGOUT = {
 LOGIN_REDIRECT_URL="home"
 LOGOUT_REDIRECT_URL="home"
 
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -229,26 +228,26 @@ DATABASES = {
         #'PORT': '3306',
 
         ################# comment out this section during deployment... it is for development ############
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'allifmysqlcommondb',# this is the name of the database
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Allif@1442',# Allif@6030114
-        'PORT': '3306',
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",}
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'allifmysqlcommondb',# this is the name of the database
+        #'HOST': 'localhost',
+        #'USER': 'root',
+        #'PASSWORD': 'Allif@1442',# Allif@6030114
+        #'PORT': '3306',
+        #'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",}
         #'PASSWORD': 'hidden',
        
         ######################3 BELOW DATABASE IS FOR PRODUCTION ############3
 
         ################# comment out this section during deployment... it is for development ############
         
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'Ahmeddove$allifmysqlcommondb',# this is the name of the database
-        #'USER': 'Ahmeddove',
-        #'PASSWORD': 'Allif@6030114',# Allif@6030114
-        #'HOST': 'Ahmeddove.mysql.pythonanywhere-services.com',
-        #'PORT': '3306',
-        #'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Ahmeddove$allifmysqlcommondb',# this is the name of the database
+        'USER': 'Ahmeddove',
+        'PASSWORD': 'Allif@6030114',# Allif@6030114
+        'HOST': 'Ahmeddove.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",}
         #DEBUG=TRUE############ SET THIS FALSE
         
         ##############3 SQLITE3 SECTION ######################3
